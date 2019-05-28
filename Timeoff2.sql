ae6231b520831698416eb93dbe14fe58ba23f66d
catalin95 <catalinmucica@gmail.com>
2019-05-28 20:57:31 +0300

    Update Timeoff2.sql

--- Timeoff2.sql
+++ Timeoff2.sql
@@ -1,25 +1,25 @@
-alter procedure TimeOff2 (@xmlUnitSN text, @xmlProdOrder text = null, @xmlPart text = null, @xmlStation text = null, 
-						  @EmployeeID int, @xmlExtraData text = null)
+alter procedure TimeOff (@xml1 text, @xml2 text = null, @xml3 text = null, @xml4 text = null, 
+						  @EID int, @xml5 text = null)
 
 as
 begin
 
-declare @UnitID int
+declare @UID int
 declare @Time_to_pass datetime 
-declare @Time_of_unit datetime 
+declare @Time_of_u datetime 
 declare @Time_to_compare_with datetime
 declare @Current_time datetime = getdate()
 declare @T1 int = 1
 declare @T2 int = 180
 
-exec uspXMLUnit @xmlUnitSN, @UnitID output
+exec XMLU @xml1, @UID output
 
-set @Time_of_unit = (select top(1) ExitTime from dbo.ffHistory where UnitID = @UnitID order by ExitTime desc)  -- last time of unit from History
+set @Time_of_unit = (select top(1) ExitTime from History where UID = @UID order by ExitTime desc)  --last exit time registered in History
 set @Time_to_compare_with = datediff(second, @Time_of_unit, @Current_time)		-- difference between last time of unit from History and current time
 
 if @Time_to_compare_with between @T1 and @T2
 begin
-	return 90040011 -- must wait 3 minutes until you can scan the next unit
+	return 900 -- must wait the difference
 end
 else
 	return 0
