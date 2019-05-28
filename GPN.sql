452e8752e3b4147924d857b5de05d3d93adfb1ba
catalin95 <catalinmucica@gmail.com>
2019-05-28 21:16:47 +0300

    Update and rename GetPN.sql to GPN.sql

---
+++ GPN.sql
@@ -0,0 +1,23 @@
+﻿use Database_name
+go
+
+alter procedure GPN  -- procedure for getting any specific x linked element to any scanned y element, must be in tables
+
+@SN varchar(20)
+@elm varchar(20) output
+
+as
+begin
+
+declare @UID int
+
+set @UID = (select U.ID from U
+	    join P on P.ID = U.PID
+	    join SN on SN.ID = U.ID
+	    where SN.Value = @SN)
+
+select @elm = (select PN from U
+	       join P on P.ID = U.PID
+	       where U.ID = @UID)
+end
+
