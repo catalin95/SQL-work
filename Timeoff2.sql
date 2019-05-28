dbb15da66fd7c45bb7e0e8c3137e05c40a1f4249
catalin95 <catalinmucica@gmail.com>
2019-05-28 20:58:50 +0300

    Update Timeoff2.sql

--- Timeoff2.sql
+++ Timeoff2.sql
@@ -6,7 +6,7 @@ begin
 
 declare @UID int
 declare @Time_to_pass datetime 
-declare @Time_of_u datetime 
+declare @Time_of datetime 
 declare @Time_to_compare_with datetime
 declare @Current_time datetime = getdate()
 declare @T1 int = 1
@@ -15,7 +15,7 @@ declare @T2 int = 180
 exec XMLU @xml1, @UID output
 
 set @Time_of_unit = (select top(1) ExitTime from History where UID = @UID order by ExitTime desc)  --last exit time registered in History
-set @Time_to_compare_with = datediff(second, @Time_of_unit, @Current_time)		-- difference between last time of unit from History and current time
+set @Time_to_compare_with = datediff(second, @Time_of, @Current_time)		-- difference between last time of unit from History and current time
 
 if @Time_to_compare_with between @T1 and @T2
 begin
