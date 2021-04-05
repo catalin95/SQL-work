323bb9986392e3a622640519182df87285600918
Catalin Mucica <catalinmucica@gmail.com>
2021-04-05 23:29:17 +0300

    Update udp_logarithm.sql

--- MySQL/udp_logarithm.sql
+++ MySQL/udp_logarithm.sql
@@ -1,12 +1,12 @@
 create procedure udp_logarithm(base int, number int, inout counter int)
 begin
 
-	declare initial int;
+    declare initial int;
     set initial = 1;
     
     while initial < number
     do
-		set initial = initial * base;
+	set initial = initial * base;
         set counter = counter + 1;
         
 	end while;
