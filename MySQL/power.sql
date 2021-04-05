0cb0a66a66d1f7b3eb77bf33fa2e3a9899aeafd6
unknown <catalinmucica@gmail.com>
2021-04-05 20:54:32 +0300

    MySQL

---
+++ MySQL/power.sql
@@ -0,0 +1,15 @@
+create procedure udp_power(inout number int, power int)
+begin
+declare base int;
+declare initial int;
+
+set base = number;
+set initial = 0;
+
+while initial != power - 1
+do
+	set number = number * base;
+    set initial = initial + 1;
+end while;
+
+end
