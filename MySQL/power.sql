0fde3df1cdb383adc512d033de52d7759032245e
Catalin Mucica <catalinmucica@gmail.com>
2021-04-05 20:58:20 +0300

    Update power.sql

--- MySQL/power.sql
+++ MySQL/power.sql
@@ -1,5 +1,8 @@
 create procedure udp_power(inout number int, power int)
 begin
+
+# calculate number raised to power
+
 declare base int;
 declare initial int;
 
@@ -8,7 +11,7 @@ set initial = 0;
 
 while initial != power - 1
 do
-	set number = number * base;
+    set number = number * base;
     set initial = initial + 1;
 end while;
 
