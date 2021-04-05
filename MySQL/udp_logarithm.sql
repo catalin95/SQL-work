18a593678ef02026737d7800152782fd16e4a10d
unknown <catalinmucica@gmail.com>
2021-04-05 23:25:23 +0300

    Procedure for calculating logarithm

---
+++ MySQL/udp_logarithm.sql
@@ -0,0 +1,14 @@
+create procedure udp_logarithm(base int, number int, inout counter int)
+begin
+
+	declare initial int;
+    set initial = 1;
+    
+    while initial < number
+    do
+		set initial = initial * base;
+        set counter = counter + 1;
+        
+	end while;
+
+end
